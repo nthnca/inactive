5fca177c12c05ade6ffb7a785c86845c44c33bdb
Nathan Bullock <nathanbullock@gmail.com>
2019-07-28 19:25:53 -0700

    Simplify the formatting for info and fatal

--- inactive.go
+++ inactive.go
@@ -27,54 +27,51 @@ var (
 	logWriter *syslog.Writer
 )
 
-func fatal(str string) {
+func infof(str string, args ...interface{}) {
 	err := logWriter.Info(str)
 	if err != nil {
-		log.Fatalf("failed to write to logger %s", err)
+		log.Fatalf(str, args...)
 	}
-	os.Exit(1)
 }
 
-func info(str string) {
-	err := logWriter.Info(str)
-	if err != nil {
-		log.Fatalf("failed to write to logger %s", err)
-	}
+func fatalf(str string, args ...interface{}) {
+	infof(fmt.Sprintf(str, args...))
+	os.Exit(1)
 }
 
 func main() {
 	var err error
 
-	logWriter, err = syslog.New(syslog.LOG_INFO, "AmIStilActive")
+	logWriter, err = syslog.New(syslog.LOG_INFO, "AmIStillActive")
 	if err != nil {
 		log.Fatalf("failed to open logger %s", err)
 	}
 
 	uptime := getUptime()
 	if uptime < minUptime {
-		fatal(fmt.Sprintf("System active. Uptime: %s", uptime))
+		fatalf("System active. Uptime: %s", uptime)
 	}
 
 	lastActivity := getLastActivityTime()
 	if lastActivity < minActivityThreshold {
-		fatal(fmt.Sprintf("System active. No activity seen for: %s",
-			lastActivity))
+		fatalf("System active. No activity seen for: %s",
+			lastActivity)
 	}
 
-	info(fmt.Sprintf("System INactive. Uptime: %s, last activity seen %s ago.\n",
-		uptime, lastActivity))
+	infof("System INactive. Uptime: %s, last activity seen %s ago.\n",
+		uptime, lastActivity)
 }
 
 func getUptime() time.Duration {
 	uptime, err := os.Open("/proc/uptime")
 	if err != nil {
-		fatal(fmt.Sprintf("Can't open /proc/uptime: %s", err))
+		fatalf("Can't open /proc/uptime: %s", err)
 	}
 
 	b := make([]byte, 50)
 	n, err := uptime.Read(b)
 	if err != nil {
-		fatal(fmt.Sprintf("Can't read /proc/uptime: %s", err))
+		fatalf("Can't read /proc/uptime: %s", err)
 	}
 
 	tmp := string(b[:n])
@@ -82,8 +79,8 @@ func getUptime() time.Duration {
 	chars := tmp[:split]
 	seconds, err := strconv.Atoi(chars)
 	if err != nil {
-		fatal(fmt.Sprintf("/proc/uptime can't be converted to an integer: %s",
-			tmp))
+		fatalf("/proc/uptime can't be converted to an integer: %s",
+			tmp)
 	}
 
 	return time.Duration(seconds) * time.Second
@@ -92,12 +89,12 @@ func getUptime() time.Duration {
 func getLastActivityTime() time.Duration {
 	f, err := os.Open(tmpFilePath)
 	if err != nil {
-		fatal(fmt.Sprintf("Can't open: %s %s", tmpFilePath, err))
+		fatalf("Can't open: %s %s", tmpFilePath, err)
 	}
 
 	fi, err := f.Readdir(2000)
 	if err != nil {
-		fatal(fmt.Sprintf("Can't readdir: %s %s", tmpFilePath, err))
+		fatalf("Can't readdir: %s %s", tmpFilePath, err)
 	}
 
 	var lastActive time.Time
